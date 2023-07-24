6debf987143daf8e78a66450cee597cbaac04d47
Alex Eum <alexeum123@gmail.com>
2023-07-24 02:05:55 -0400

    lets go

--- Training/Trainingmore/Trainingmore.js
+++ Training/Trainingmore/Trainingmore.js
@@ -1,11 +1,10 @@
-// Animate Smooth Scroll
-$('#view-work').on('click', function() {
-    const landing = $('#landing').position().top;
+  // Animate Smooth Scroll
+$('#view-work').on('click', function () {
+  const images = $('#images').position().top;
 
-    $('html, body').animate(
-      {
-        scrollTop: landing
-      },
-      900
-    );
-  });
+  $('html, body').animate({
+      scrollTop: images
+    },
+    900
+  );
+});

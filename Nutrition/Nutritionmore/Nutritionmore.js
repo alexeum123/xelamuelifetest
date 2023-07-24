bd3ffcabc539defcceab1a15a6e00b83582bf813
Alex Eum <alexeum123@gmail.com>
2023-07-24 02:15:28 -0400

    working trainingmore

--- Nutrition/Nutritionmore/Nutritionmore.js
+++ Nutrition/Nutritionmore/Nutritionmore.js
@@ -1,10 +1,9 @@
-// Animate Smooth Scroll
-$('#view-work').on('click', function() {
-    const landing = $('#landing').position().top;
-
-    $('html, body').animate(
-      {
-        scrollTop: landing
+  // Animate Smooth Scroll
+  $('#view-work').on('click', function () {
+    const images = $('#images').position().top;
+  
+    $('html, body').animate({
+        scrollTop: images
       },
       900
     );
